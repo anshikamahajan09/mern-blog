0c46f899b855461b5fdcbde574a80fa5dbbae759
anshikamahajan09 <anshika1304.be22@chitkara.edu.in>
2024-04-10 14:45:19 +0530

    complete create post api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -61,7 +61,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id,isAdmin:user.isAdmin }, process.env.JWT_SECRET);
       const { password: hashedPassword, ...rest } = user._doc;
       res
         .status(200)
