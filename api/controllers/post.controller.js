21e8a1278afa6ae687ca9b029430cee0983f7275
anshikamahajan09 <anshika1304.be22@chitkara.edu.in>
2024-04-10 19:20:43 +0530

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,8 +1,9 @@
+import { parse } from "dotenv";
 import Post from "../models/post.model.js";
-import {errorHandler} from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-    console.log(req.user);
+  console.log(req.user);
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
   }
@@ -17,12 +18,54 @@ export const create = async (req, res, next) => {
   const newPost = new Post({
     ...req.body,
     slug,
-    userId:req.user.id
-});
-    try {
-        const savedPost = await newPost.save();
-        res.status(201).json(savedPost);
-    } catch (error) {
-        next(error);
-    }
-    }
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.category && { title: req.query.category }),
+      ...(req.query.postId && { postId: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo},
+    });
+
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+
+  } catch (error) {
+    next(error);
+  }
+};
